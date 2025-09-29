375d585e7523fb5ce63625771d21943be9dbeb02
Jorge Garcia <garciajorgecf@gmail.com>
2025-09-29 01:11:14 -0600

    Añadiendo conexión a supabase

--- ninerapp/lib/main.dart
+++ ninerapp/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ninerapp/screens/main_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  String supabaseUrl = dotenv.env['SUPABASE_URL']!;
+  String supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY']!;
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(const MainApp());
 }
 
