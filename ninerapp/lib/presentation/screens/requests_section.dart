758e314b208ea47ae61e3d3f3da269a8581cc008
Jorge Garcia <garciajorgecf@gmail.com>
2025-10-01 22:55:35 -0600

    cambiando history_section por requests_section

---
+++ ninerapp/lib/presentation/screens/requests_section.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RequestsSection extends StatefulWidget {
+  const RequestsSection({super.key});
+
+  @override
+  State<RequestsSection> createState() => _RequestsSectionState();
+}
+
+class _RequestsSectionState extends State<RequestsSection> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
