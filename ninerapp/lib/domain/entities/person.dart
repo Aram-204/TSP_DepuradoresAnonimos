a86c6df53908a8f59bf53ff0bbd5284b19540442
Jorge Garcia <garciajorgecf@gmail.com>
2025-10-01 22:54:36 -0600

    Creando clase padre de person para ser heredada por niñeros, hijos, y tutores

---
+++ ninerapp/lib/domain/entities/person.dart
@@ -0,0 +1,33 @@
+class Person {
+  final int? id;
+  final String name;
+  final String lastName;
+  final DateTime birthdate;
+  final bool isFemale;
+
+  Person({
+    this.id,
+    required this.name,
+    required this.lastName,
+    required this.birthdate,
+    required this.isFemale,
+  });
+
+  int getAge() {
+    final currentDate = DateTime.now();
+    int age = currentDate.year - birthdate.year;
+    if (currentDate.month < birthdate.month || (currentDate.month == birthdate.month && currentDate.day < birthdate.day)) {
+      age--;
+    }
+    return age;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'last_name': lastName,
+      'birthdate': birthdate.toIso8601String(),
+      'is_female': isFemale,
+    };
+  }
+}
