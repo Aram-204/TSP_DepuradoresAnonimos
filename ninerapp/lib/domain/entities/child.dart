3c542fb674eadff7ffb4cb59828949ceecb6773b
Jorge Garcia <garciajorgecf@gmail.com>
2025-10-01 22:54:11 -0600

    Haciendo que child herede de person

--- ninerapp/lib/domain/entities/child.dart
+++ ninerapp/lib/domain/entities/child.dart
@@ -1,60 +1,44 @@
-class Child {
-  final int? id;
-  final String name;
-  final String lastName;
-  final DateTime birthdate;
-  final bool isFemale;
+import 'package:ninerapp/domain/entities/person.dart';
+
+class Child extends Person {
   final bool disabilityFisica;
   final bool disabilityAuditiva;
   final bool disabilityVisual;
-  final String? otherDisability;
+  final String? otherDisabilities;
 
   Child({
-    this.id,
-    required this.name,
-    required this.lastName,
-    required this.birthdate,
-    required this.isFemale,
+    super.id,
+    required super.name,
+    required super.lastName,
+    required super.birthdate,
+    required super.isFemale,
     required this.disabilityFisica,
     required this.disabilityAuditiva,
     required this.disabilityVisual,
-    required this.otherDisability,
+    this.otherDisabilities,
   });
 
-  int getAge() {
-    final currentDate = DateTime.now();
-    int age = currentDate.year - birthdate.year;
-    if (currentDate.month < birthdate.month ||
-        (currentDate.month == birthdate.month && currentDate.day < birthdate.day)) {
-      age--;
-    }
-    return age;
-  }
-
-  static Child fromMap(Map<String, dynamic> json) {
+  static Child fromMap(Map<String, dynamic> map) {
     return Child(
-      id: json['id'] as int,
-      name: json['name'] as String,
-      lastName: json['last_name'] as String,
-      birthdate: DateTime.parse(json['birthdate'] as String),
-      isFemale: json['is_female'] as bool,
-      disabilityFisica: json['disability_fisica'] as bool,
-      disabilityAuditiva: json['disability_auditiva'] as bool,
-      disabilityVisual: json['disability_visual'] as bool,
-      otherDisability: json['other_disability'] as String?,
+      id: map['id'] as int,
+      name: map['name'] as String,
+      lastName: map['last_name'] as String,
+      birthdate: DateTime.parse(map['birthdate'] as String),
+      isFemale: map['is_female'] as bool,
+      disabilityFisica: map['phisical_disability'] as bool,
+      disabilityAuditiva: map['hearing_disability'] as bool,
+      disabilityVisual: map['visual_disability'] as bool,
+      otherDisabilities: map['other_disability'] as String?,
     );
   }
 
+  @override
   Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'last_name': lastName,
-      'birthdate': birthdate.toIso8601String(),
-      'is_female': isFemale,
-      'disability_fisica': disabilityFisica,
-      'disability_auditiva': disabilityAuditiva,
-      'disability_visual': disabilityVisual,
-      'other_disability': otherDisability,
-    };
+    return super.toMap()..addAll({
+      'phisical_disability': disabilityFisica,
+      'hearing_disability': disabilityAuditiva,
+      'visual_disability': disabilityVisual,
+      'other_disabilities': otherDisabilities,
+    });
   }
 }
